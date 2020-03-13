690a556851ceafe678251935e9408166011aa510
Naofumi Okamoto <nokamoto.engr@gmail.com>
2020-03-13 18:08:04 +0900

    Append upstream metadata (#5)

--- cmd/echo/echo.go
+++ cmd/echo/echo.go
@@ -3,11 +3,16 @@ package main
 import (
 	"context"
 	"github.com/nokamoto/envoy-external-authz/protobuf"
+	"google.golang.org/grpc/metadata"
+	"log"
 )
 
 type Server struct{}
 
 func (*Server) Echo(ctx context.Context, req *protobuf.EchoRequest) (*protobuf.EchoResponse, error) {
+	md, found := metadata.FromIncomingContext(ctx)
+	log.Printf("metadata=%v, found=%v", md, found)
+
 	return &protobuf.EchoResponse{
 		Value: req.GetValue(),
 	}, nil
